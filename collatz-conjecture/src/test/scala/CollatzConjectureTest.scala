ef645ec70635b8ce657554aeb35816a5332d3051
Vyun Sergey <vyun.sergey@icloud.com>
2023-03-19 13:28:23 +0300

    add more tests

--- collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
+++ collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
@@ -10,27 +10,26 @@ class CollatzConjectureTest extends AnyFunSuite with Matchers {
   }
 
   test("divide if even") {
-    pending
     CollatzConjecture.steps(16) should be (Some(4))
   }
 
   test("even and odd steps") {
-    pending
     CollatzConjecture.steps(12) should be (Some(9))
   }
 
   test("Large number of even and odd steps") {
-    pending
     CollatzConjecture.steps(1000000) should be (Some(152))
   }
 
+  test("Integer Max Value") {
+    CollatzConjecture.steps(Int.MaxValue) should be(Some(450))
+  }
+
   test("zero is an error") {
-    pending
     CollatzConjecture.steps(0) should be (None)
   }
 
   test("negative value is an error") {
-    pending
     CollatzConjecture.steps(-15) should be (None)
   }
 }
